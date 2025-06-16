6a92451ec96e85f8808828fc2fb72b205e646eba
ardur10v <1032221511@tcetmumbai.in>
2025-06-16 20:39:09 +0530

    cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://chatx-zdke.onrender.com/"]
     credentials:true
 }))
 app.use("/api/auth",authRoutes);
