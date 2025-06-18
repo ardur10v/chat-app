d5f7344318cabf1dda8df2d6a0f0978c9a7abc97
ardur10v <1032221511@tcetmumbai.in>
2025-06-18 14:19:30 +0530

    navv

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { useAuthStore } from '../store/useAuthStore'
 import { MessageSquare, Settings, User, LogOut } from 'lucide-react'
 
 const Navbar = () => {
-    const { logout, authuser } = useAuthStore()
-    console.log(authuser);
+    const { logout, authUser } = useAuthStore()
+    console.log(authUser);
     return (
         <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80 ">
             <div className="container mx-auto px-4 h-16">
@@ -25,7 +25,7 @@ const Navbar = () => {
                             <Settings className="w-4 h-4" />
                             <span className="hidden sm:inline">Settings</span>
                         </Link>
-                        {authuser && (
+                        {authUser && (
                             <>
                                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
                                     <User className="size-5" />
