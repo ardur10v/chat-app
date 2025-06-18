b696681cc5030b308963bcd676a55bb415a5a327
ardur10v <1032221511@tcetmumbai.in>
2025-06-18 16:51:12 +0530

    auth

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import ChatContainer from "../components/ChatContainer";
 import NoChatSelected from "../components/NoChatSelected";
-import { useChatStore } from "../store/useChatStore"
+import { useChatStore } from "../store/useChatStore";
+import {useAuthStore} from "../store/useAuthStore";
 import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const HomePage = () => {
   const {selectedUser} =useChatStore();
+  const navigate=useNavigate();
+  const {authUser,isCheckingAuth} =useAuthStore();
+  useEffect(()=>{
+    if(!isCheckingAuth && !authUser){
+      navigate("/signup");
+    }
+  })
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
