d3c726dcda3ac54957a0d3133783e04e78f8b44c
ardur10v <1032221511@tcetmumbai.in>
2025-06-18 16:52:13 +0530

    auth

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,17 +1,17 @@
 import ChatContainer from "../components/ChatContainer";
 import NoChatSelected from "../components/NoChatSelected";
 import { useChatStore } from "../store/useChatStore";
-import {useAuthStore} from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import Sidebar from "../components/Sidebar";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const HomePage = () => {
-  const {selectedUser} =useChatStore();
-  const navigate=useNavigate();
-  const {authUser,isCheckingAuth} =useAuthStore();
-  useEffect(()=>{
-    if(!isCheckingAuth && !authUser){
+  const { selectedUser } = useChatStore();
+  const navigate = useNavigate();
+  const { authUser, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    if (!isCheckingAuth && !authUser) {
       navigate("/signup");
     }
   })
@@ -20,9 +20,9 @@ const HomePage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar/>
-            {!selectedUser?<NoChatSelected/>:<ChatContainer></ChatContainer>}
-            
+            <Sidebar />
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer></ChatContainer>}
+
           </div>
         </div>
       </div>
