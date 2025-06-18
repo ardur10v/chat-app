7584ae613e1389ddb340df9d6ca6e1bd5255863b
ardur10v <1032221511@tcetmumbai.in>
2025-06-18 14:41:24 +0530

    navviii

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,10 +31,10 @@ const Navbar = () => {
                                     <User className="size-5" />
                                     <span className="hidden sm:inline">Profile</span>
                                 </Link>
-                                <button className="flex gap-2 items-center" onClick={logout}>
+                                <Link to={"/login"} className="flex gap-2 items-center" onClick={logout}>
                                     <LogOut className="size-5" />
                                     <span className="hidden sm:inline">Logout</span>
-                                </button>
+                                </Link>
                             </>
                         )}
                     </div>
